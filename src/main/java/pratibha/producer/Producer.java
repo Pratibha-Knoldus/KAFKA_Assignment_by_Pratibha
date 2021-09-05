432455a31fe45d250d4686c972767f2a8d2a2e85
Pratibha-Knoldus <pratibha.yadav@knoldus.com>
2021-09-05 17:58:01 +0530

    kafka assignment

---
+++ src/main/java/pratibha/producer/Producer.java
@@ -0,0 +1,33 @@
+package pratibha.producer;
+
+import pratibha.model.User;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.Random;
+
+public class Producer {
+    public static void main(String[] args){
+
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "pratibha.serializer.UserSerializer");
+        KafkaProducer<String, User> producer = new KafkaProducer<>(properties);
+        try {
+            Random rand = new Random();
+            for (int i = 1; i <= 100; i++) {
+
+                User user = new User(i, "Pratibha", rand.nextInt(10)+20, "MCA");
+                producer.send(new ProducerRecord<String, User>("userobj", String.valueOf(i), user));
+                System.out.println("Message " + user.toString() + " sent !!");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            producer.close();
+        }
+    }
+}
